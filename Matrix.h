fb5e2e540b0ed4c9f9f89adb98f6a0d221e6aefb
misheyn <am.dolmatova01@gmail.com>
2021-09-22 17:29:02 +0700

    add comments

--- Matrix.h
+++ Matrix.h
@@ -9,32 +9,33 @@
 class Matrix {
 
 public:
+    // конструктор по умолчанию
     Matrix();
-
+    // конструктор с параметрами
     Matrix(int order);
-
+    // конструктор копирования
     Matrix(const Matrix &matrix_2);
-
+    // деструктор
     ~Matrix();
-
+    // метод получения значения матрицы
     int GetMatrix(int i, int j) const;
-
+    // метод устанавливающий значения матрицы
     void SetMatrix(int i, int j, int value);
-
+    // метод получения матрицы в строковом представлении
     char *toString() const;
-
+    // метод транспонирования матрицы
     void TransposeMatrix();
-
+    // перегруженный оператор сложения
     Matrix operator+(const Matrix &matrix_2);
-
+    // перегруженный оператор присваивания
     Matrix &operator=(const Matrix &matrix_2);
 
 private:
-    int order;
-    int **matrix;
-
+    int order; // поле порядка матрицы
+    int **matrix; // поле набора элементов матрицы
+    // статичный метод сохранения символов в массив символов
     static int strCat(char *buf, int len, char elem);
-
+    // статичный метод подсчета количества цифр в числе
     static int digitCount(int **_matrix, int _order);
 };
 
