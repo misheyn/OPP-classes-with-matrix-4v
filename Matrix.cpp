870d51b2ad9c1c38fdc0cb915d996054f69b7c5e
misheyn <am.dolmatova01@gmail.com>
2021-10-13 18:08:27 +0700

    streams fix and again done

--- Matrix.cpp
+++ Matrix.cpp
@@ -14,28 +14,27 @@ Matrix::Matrix() {
 }
 
 Matrix::Matrix(int order) {
-    if (order < 1 || isalpha((char)order)) throw invalid_argument("Incorrect matrix order");
+    if (order < 1 || isalpha((char) order))
+        throw invalid_argument("Invalid matrix order");
     this->order = order;
     this->matrix = new double *[order];
-    //if (this->matrix == nullptr) throw "Memory denied";
     for (int i = 0; i < order; i++) {
         this->matrix[i] = new double[order];
-        //if (this->matrix[i] == nullptr) throw "Memory denied";
     }
     for (int i = 0; i < order; i++)
         for (int j = 0; j < order; j++)
             this->matrix[i][j] = 0;
 }
 
-Matrix::Matrix(const Matrix &matrix_2) {
-    this->order = matrix_2.order;
-    this->matrix = new double *[matrix_2.order];
+Matrix::Matrix(const Matrix &obj) {
+    this->order = obj.order;
+    this->matrix = new double *[obj.order];
     for (int i = 0; i < order; i++) {
-        this->matrix[i] = new double[matrix_2.order];
+        this->matrix[i] = new double[obj.order];
     }
-    for (int i = 0; i < matrix_2.order; i++)
-        for (int j = 0; j < matrix_2.order; j++)
-            this->matrix[i][j] = matrix_2.matrix[i][j];
+    for (int i = 0; i < obj.order; i++)
+        for (int j = 0; j < obj.order; j++)
+            this->matrix[i][j] = obj.matrix[i][j];
 }
 
 Matrix::~Matrix() {
@@ -46,10 +45,14 @@ Matrix::~Matrix() {
 }
 
 double Matrix::GetMatrix(int i, int j) const {
+    if (i >= this->order || j >= this->order || i < 0 || j < 0)
+        throw out_of_range("Out of range matrix");
     return this->matrix[i][j];
 }
 
 void Matrix::SetMatrix(int i, int j, int value) {
+    if (i >= this->order || j >= this->order || i < 0 || j < 0)
+        throw out_of_range("Out of range matrix");
     this->matrix[i][j] = value;
 }
 
@@ -115,6 +118,8 @@ int Matrix::digitCount(double **_matrix, int _order) {
 }
 
 Matrix operator+(const Matrix &m_1, const Matrix &m_2) {
+    if (m_2.order != m_1.order || m_2.order > m_1.order || m_2.order < m_1.order)
+        throw logic_error("Add/subtract matrices of different orders");
     Matrix temp(m_1.order);
     for (int i = 0; i < m_1.order; i++) {
         for (int j = 0; j < m_1.order; j++) {
@@ -125,6 +130,8 @@ Matrix operator+(const Matrix &m_1, const Matrix &m_2) {
 }
 
 Matrix operator-(const Matrix &m_1, const Matrix &m_2) {
+    if (m_2.order != m_1.order)
+        throw logic_error("Add/subtract matrices of different orders");
     Matrix temp(m_1.order);
     for (int i = 0; i < m_1.order; i++) {
         for (int j = 0; j < m_1.order; j++) {
@@ -155,6 +162,8 @@ Matrix &Matrix::operator=(const Matrix &new_m) {
 }
 
 double &Matrix::operator()(int i, int j) {
+    if (i > this->order || j > this->order || i < 0 || j < 0)
+        throw out_of_range("Out of range matrix");
     return this->matrix[i][j];
 }
 
@@ -190,4 +199,29 @@ double Matrix::operator()() {
     delete[] _matrix;
 
     return det;
+}
+
+ostream &operator<<(ostream &os, const Matrix &m) {
+    for (int i = 0; i < m.order; ++i) {
+        for (int j = 0; j < m.order; ++j) {
+            os << m.matrix[i][j];
+            if (j != m.order - 1) os << " ";
+        }
+        if (i != m.order - 1) os << "\n";
+        else os << "";
+    }
+    return os;
+}
+
+istream &operator>>(istream &is, Matrix &m) {
+    m.matrix = new double *[m.order];
+    for (int i = 0; i < m.order; i++) {
+        m.matrix[i] = new double[m.order];
+    }
+    for (int i = 0; i < m.order; ++i) {
+        for (int j = 0; j < m.order; ++j) {
+            is >> m.matrix[i][j];
+        }
+    }
+    return is;
 }
