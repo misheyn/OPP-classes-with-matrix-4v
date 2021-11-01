3856f1adace04953867a7d5219b1c1c13771563b
misheyn <am.dolmatova01@gmail.com>
2021-11-01 22:26:41 +0700

    final 1

--- Queue.h
+++ Queue.h
@@ -6,18 +6,59 @@
 #define LAB1_3_QUEUE_H
 
 #include "Node.h"
-template <typename T>
+#include "Matrix.h"
+
+template<typename T>
 
 class Queue {
 public:
 
-    Queue();
+    Queue() : first(nullptr) {}
 
-    void pushEnd(T &ob);
+    void pushEnd(T &elem) {
+        auto *tmp = new node(elem);
+        if (first != nullptr) {
+            if (first->prev != nullptr) {
+                first->prev->next = tmp;
+                tmp->prev = first->prev;
+                tmp->next = first;
+                first->prev = tmp;
+            } else {
+                first->next = tmp;
+                first->prev = tmp;
+                tmp->next = first;
+                tmp->prev = first;
+            }
 
-    void popBegin();
+        } else {
+            first = tmp;
+        }
+    }
 
-    T &operator[](int i);
+    void popBegin() {
+        node<T> *tmp = first;
+        if (first != nullptr) {
+            if (first->prev != nullptr) {
+                first->prev->next = first->next;
+                first->next->prev = first->prev;
+                first = first->next;
+                delete tmp;
+            } else {
+                delete first;
+                first = nullptr;
+            }
+        }
+    }
+
+    T &operator[](int i) {
+        node<T> *tmp = first;
+        if (first != nullptr) {
+            for (int j = 0; j < i; j++) {
+                tmp = tmp->next;
+            }
+        }
+        return tmp->obj;
+    }
 
 private:
     node<T> *first;
